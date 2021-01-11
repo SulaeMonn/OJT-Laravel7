d3ce831694317b185d3821a2e4e0df59778ed4c0
NWENWEWINTHANT-\Nwe Nwe Win Thant <scm.nwenwewinthan@gmail.com>
2021-01-11 14:10:21 +0630

    add service dao layer

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
- 
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -21,7 +21,7 @@ Auth::routes();
 
 Route::get('/home', 'PostController@index')->name('home');
 
-Route::resource('posts','PostController');
+Route::resource('posts', 'PostController');
 Route::post('posts/confirm', 'PostController@confirm')->name('posts.confirm');
 Route::post('posts/{id}/editConfirm', 'PostController@editConfirm')->name('posts.editConfirm');
 Route::get('posts-search', 'PostController@search')->name('posts.search');
@@ -29,8 +29,8 @@ Route::get('posts-upload', 'PostController@upload')->name('posts.upload');
 Route::get('importExportView', 'PostController@importExportView');
 Route::get('export', 'PostController@export')->name('export');
 Route::post('import', 'PostController@import')->name('import');
- 
-Route::resource('users','UserController');
+
+Route::resource('users', 'UserController');
 Route::post('users/confirm', 'UserController@confirm')->name('users.confirm');
 Route::post('users/{id}/editConfirm', 'UserController@editConfirm')->name('users.editConfirm');
 Route::get('users-search', 'UserController@search')->name('users.search');
@@ -41,3 +41,5 @@ Route::post('change-password/{id}', 'UserController@passwordUpdate')->name('chan
 Route::get('/email', 'EmailController@create');
 Route::post('/email', 'EmailController@sendEmail')->name('send.email');
 
+// test route for service_dao_structure
+Route::get('/test', TestController::class . '@getList');
